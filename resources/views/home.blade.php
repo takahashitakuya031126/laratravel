edd79dcd48fa81f2c417bfead024fbe1b8de6fe7
Takahashi Takuya <takahashitakuya031126@gmail.com>
2020-06-29 14:24:43 +0900

    create views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,44 @@
-@extends('layouts.app')
+@extends('index')
+@section('title', 'Home')
 
 @section('content')
+<div class="jumbotron text-light" style="background-image: url('https://cdn.auth0.com/blog/laravel-6-crud/laravel-beach-bg.png')">
+    <div class="container">
+        @if(Auth::user())
+        <h1 class="display-4">Welcome back, {{ Auth::user()->nickname}}!</h1>
+        <p class="lead">To your one stop shop for reservation management.</p>
+        <a href="/dashboard" class="btn btn-success btn-lg my-2">View your Dashboard</a>
+        @else
+        <h1 class="display-3">Reservation management made easy.</h1>
+        <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam in quia natus magnam ducimus quas molestias velit vero maiores. Eaque sunt laudantium voluptas. Fugiat molestiae ipsa delectus iusto vel quod.</p>
+        <a href="/login" class="btn btn-success btn-lg my-2">Sign Up for Access to Thousands of Hotels</a>
+        @endif
+    </div>
+</div>
+
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="col-sm-4">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <h5 class="card-title">Convenient</h5>
+                    <p class="card-text">Manage all your hotel reservations in one place</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Best prices</h5>
+                    <p class="card-text">We have special discounts at the best hotels</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Easy to use</h5>
+                    <p class="card-text">Book and manage with the click of a button</p>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'home');
+Route::get('/hotels', 'HotelController@index');
+Route::get('/auth0/callback', '\Auth0\Login\Auth0Controller@callback' )->name('auth0-callback');
+Route::get('/login', 'Auth\Auth0IndexController@login')->name('login');
+Route::get('/logout', 'Auth\Auth0IndexController@logout')->name('logout')->middleware('auth');
+
+Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], function() {
+    Route::view('/', 'dashboard/dashboard');
+    Route::get('reservations/create/{id}', 'ReservationController@create');
+    Route::resource('reservations', 'ReservationController')->except('create');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
